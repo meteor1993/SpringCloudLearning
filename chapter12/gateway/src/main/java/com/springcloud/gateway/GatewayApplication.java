1efbded8ec1082ee4ad38f2a35eb55d6b6ea0945
weishiyao <inwsy@hotmail.com>
2019-07-09 23:23:55 +0800

    chapter12

---
+++ chapter12/gateway/src/main/java/com/springcloud/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+//	@Bean
+//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route("path_route", r -> r.path("/meteor_93")
+//						.uri("https://blog.csdn.net"))
+//				.build();
+//	}
+
+}
