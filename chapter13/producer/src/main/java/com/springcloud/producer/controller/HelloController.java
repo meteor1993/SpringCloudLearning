262620690aa72910b9b76eb58dc7f75c992d99c0
weishiyao <inwsy@hotmail.com>
2019-07-11 19:06:45 +0800

    chapter12

---
+++ chapter13/producer/src/main/java/com/springcloud/producer/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.springcloud.producer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Date: 2019/7/2
+ * @Time: 0:02
+ * @email: inwsy@hotmail.com
+ * Description:
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "hello "+name+"，producer is ready";
+    }
+
+    @RequestMapping("/foo")
+    public String foo(String foo) {
+        return "hello "+foo+"!";
+    }
+}
