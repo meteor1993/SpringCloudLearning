4803440ab756d67475396d8c70d544fcec717994
weishiyao <inwsy@hotmail.com>
2019-07-14 18:25:55 +0800

    Nacos配置管理

---
+++ Alibaba/nacos-config/src/main/java/com/springcloud/nacosconfig/NacosConfigApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.nacosconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosConfigApplication.class, args);
+	}
+
+}
---
+++ Alibaba/nacos-config/src/main/java/com/springcloud/nacosconfig/controller/ConfigController.java
@@ -0,0 +1,28 @@
+package com.springcloud.nacosconfig.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Date: 2019/7/14
+ * @Time: 18:13
+ * @email: inwsy@hotmail.com
+ * Description:
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
