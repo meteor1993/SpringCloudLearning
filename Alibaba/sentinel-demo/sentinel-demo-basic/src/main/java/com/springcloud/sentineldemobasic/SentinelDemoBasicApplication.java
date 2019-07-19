1058d3e1084f8a1f8bb9c93cef790e099d8b2eb1
inwsy <inwsy@hotmail.com>
2019-07-19 17:02:04 +0800

    Spring Cloud Alibaba | Sentinel: 服务限流高级篇

---
+++ Alibaba/sentinel-demo/sentinel-demo-basic/src/main/java/com/springcloud/sentineldemobasic/SentinelDemoBasicApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.sentineldemobasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelDemoBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelDemoBasicApplication.class, args);
+	}
+
+}
