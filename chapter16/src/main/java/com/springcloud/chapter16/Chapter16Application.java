2262a7724eb00a0db0be537777cf736dd3dd7a37
inwsy <inwsy@hotmail.com>
2019-09-06 01:21:21 +0800

    update chapter16

--- chapter16/src/main/java/com/springcloud/chapter16/Chapter16Application.java
+++ chapter16/src/main/java/com/springcloud/chapter16/Chapter16Application.java
@@ -1,9 +1,15 @@
 package com.springcloud.chapter16;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableApolloConfig
 public class Chapter16Application {
 
     public static void main(String[] args) {
